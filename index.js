bf280d507fc38431017b344b4962a499f0f1aa4f
ThisisAlam <103932559+ThisisAlam@users.noreply.github.com>
2026-04-06 04:56:50 +0500

    updated CSS

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+// Data Improted into the Javascript //
 import { tweetsData } from './data.js'
 
-console.log(tweetsData)
+// Taking Control of ID's //
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
 
-/*
-Challenge:
-1. Put all of the data in its own file called
-   data.js, and export it back into index.js. 
-   Make any changes to index.html that are
-   necessary to make this work.
-2. Log out tweetsData.
-*/
+// Listening to Event //
+tweetBtn.addEventListener('click', function(){
+   console.log(tweetInput.value)
+})
